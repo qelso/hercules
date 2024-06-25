62cece81b4dbefbb6d05b464d169368991f14c11
Alessio Jin <alessioqj@gmail.com>
2024-06-25 12:16:06 +0200

    Fix handling of unloading audio from memory

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -2,97 +2,62 @@ import { StyleSheet, Pressable } from "react-native"
 import React, { useEffect, useState, useRef } from "react"
 import { Text, useTheme, Button, ActivityIndicator } from "react-native-paper"
 import { View } from "react-native"
-import { Audio } from "expo-av"
-import { Sound } from "expo-av/build/Audio"
-import { addWhitelistedNativeProps } from "react-native-reanimated/lib/typescript/ConfigHelper"
-import { AuthSessionOpenOptions } from "expo-web-browser"
+import { Audio, AVPlaybackStatus, AVPlaybackStatusSuccess } from "expo-av"
+
 
 export default function Timer({ duration, preTimer = 10 }: { duration: number, preTimer: number }) {
     const [seconds, setSeconds] = useState<number>(duration)
     const [isRunning, setIsRunning] = useState(false)
     const [preSeconds, setPreSeconds] = useState(preTimer)
     const [isPreTimer, setIsPreTimer] = useState( preTimer > 0 ? true : false) 
-    const wasRunning = usePrevious(isRunning)
-   
-    const[getReadySound,setGetReadySound] = useState<Audio.Sound>()
-    const[letsGoSound,setLetsGoSound] = useState<Audio.Sound>()
-    const[wellDoneSound, setWellDoneSound] = useState<Audio.Sound>()
-    const[fiveSound, setFiveSound] = useState<Audio.Sound>()
-    const[fourSound, setFourSound] = useState<Audio.Sound>()
-    const[threeSound, setThreeSound] = useState<Audio.Sound>()
-    const[twoSound, setTwoSound] = useState<Audio.Sound>()
-    const[oneSound, setOneSound] = useState<Audio.Sound>() 
 
     async function playGetReadySound() {
-        const {sound} = await Audio.Sound.createAsync(require("../assets/sounds/get_ready.mp3"))
-        setGetReadySound(sound)
+        const {sound} = await Audio.Sound.createAsync(require("../assets/sounds/get_ready.mp3"),{},(status) => {if(status.didJustFinish) sound.unloadAsync()})
+        sound.setOnPlaybackStatusUpdate((status) => {
+        })
         await sound.playAsync()  
     }
     async function playLetsGoSound() {
-        const {sound} = await Audio.Sound.createAsync(require("../assets/sounds/lets_go.mp3"))
-        setLetsGoSound(sound)
+        const {sound} = await Audio.Sound.createAsync(require("../assets/sounds/lets_go.mp3"),{},(status) => {if(status.didJustFinish) sound.unloadAsync()})
         await sound.playAsync()  
     }
     async function playWellDoneSound() {
-        const {sound} = await Audio.Sound.createAsync(require("../assets/sounds/well_done.mp3"))
-        setWellDoneSound(sound)
+        const {sound} = await Audio.Sound.createAsync(require("../assets/sounds/well_done.mp3"),{},(status) => {if(status.didJustFinish) sound.unloadAsync()})
         await sound.playAsync()  
     }
     async function playFiveSound() {
-        const {sound} = await Audio.Sound.createAsync(require("../assets/sounds/five.mp3"))
-        setFiveSound(sound)
+        const {sound} = await Audio.Sound.createAsync(require("../assets/sounds/five.mp3"),{},(status) => {if(status.didJustFinish) sound.unloadAsync()})
         await sound.playAsync()  
     }
     async function playFourSound() {
-        const {sound} = await Audio.Sound.createAsync(require("../assets/sounds/four.mp3"))
-        setFourSound(sound)
+        const {sound} = await Audio.Sound.createAsync(require("../assets/sounds/four.mp3"),{},(status) => {if(status.didJustFinish) sound.unloadAsync()})
         await sound.playAsync()  
     }
     async function playThreeSound() {
-        const {sound} = await Audio.Sound.createAsync(require("../assets/sounds/three.mp3"))
-        setThreeSound(sound)
+        const {sound} = await Audio.Sound.createAsync(require("../assets/sounds/three.mp3"),{},(status) => {if(status.didJustFinish) sound.unloadAsync()})
         await sound.playAsync()  
     }
 
     async function playTwoSound() {
-        const {sound} = await Audio.Sound.createAsync(require("../assets/sounds/two.mp3"))
-        setTwoSound(sound)
+        const {sound} = await Audio.Sound.createAsync(require("../assets/sounds/two.mp3"),{},(status) => {if(status.didJustFinish) sound.unloadAsync()})
         await sound.playAsync()  
     }
 
     async function playOneSound() {
-        const {sound} = await Audio.Sound.createAsync(require("../assets/sounds/one.mp3"))
-        setOneSound(sound)
+        const {sound} = await Audio.Sound.createAsync(require("../assets/sounds/one.mp3"),{},(status) => {if(status.didJustFinish) sound.unloadAsync()})
+        await sound.playAsync()  
+    }
+    
+    async function playThirtySound() {
+        const {sound} = await Audio.Sound.createAsync(require("../assets/sounds/thirty_seconds.mp3"),{},(status) => {if(status.didJustFinish) sound.unloadAsync()})
         await sound.playAsync()  
     }
 
-    useEffect(() => {
-        letsGoSound ? () => {letsGoSound.unloadAsync()} : undefined
-    },[letsGoSound])
-    useEffect(() => {
-        getReadySound ? () => {getReadySound.unloadAsync()} : undefined
-    },[getReadySound])
-    useEffect(() => {
-        wellDoneSound ? () => {wellDoneSound.unloadAsync()} : undefined
-    },[wellDoneSound]) 
-    useEffect(() => {
-        fiveSound ? () => {fiveSound.unloadAsync()} : undefined
-    },[fiveSound]) 
-    useEffect(() => {
-        fourSound ? () => {fourSound.unloadAsync()} : undefined
-    },[fourSound]) 
-    useEffect(() => {
-        threeSound ? () => {threeSound.unloadAsync()} : undefined
-    },[threeSound]) 
-    useEffect(() => {
-        threeSound ? () => {threeSound.unloadAsync()} : undefined
-    },[threeSound]) 
-    useEffect(() => {
-        twoSound ? () => {twoSound.unloadAsync()} : undefined
-    },[twoSound]) 
-    useEffect(() => {
-        oneSound ? () => {oneSound.unloadAsync()} : undefined
-    },[oneSound]) 
+    async function playTenSound() {
+        const {sound} = await Audio.Sound.createAsync(require("../assets/sounds/ten_seconds.mp3"),{},(status) => {if(status.didJustFinish) sound.unloadAsync()})
+        await sound.playAsync()  
+    }
+    
 
     useEffect(() => {
         let interval: number | NodeJS.Timeout | undefined;
@@ -120,7 +85,7 @@ export default function Timer({ duration, preTimer = 10 }: { duration: number, p
                                     break
                                 case 1:
                                     playOneSound()
-                                    break
+                                    break 
                             }
                             return prev - 1
                         }
@@ -135,10 +100,40 @@ export default function Timer({ duration, preTimer = 10 }: { duration: number, p
                 else {
                 setSeconds(seconds => {
                     if (seconds > 0) {
+                        switch ((seconds-1) % 60) {
+                            case 30:
+                                playThirtySound()
+                                break
+                            case 10:
+                                playTenSound()
+                                break
+                            case 5:
+                                playFiveSound()
+                                break
+                            case 4:
+                                playFourSound()
+                                break
+                            case 3:
+                                playThreeSound()
+                                break
+                            case 2:
+                                playTwoSound()
+                                break
+                            case 1:
+                                playOneSound()
+                                break
+                            case 0:
+                                if (seconds-1 > 0) playLetsGoSound()
+                        }
                         return seconds - 1
                     }
                     else {
                         clearInterval(interval)
+                        playWellDoneSound()
+                        setIsPreTimer(true)
+                        setPreSeconds(preTimer)
+                        setIsRunning(false)
+                        setSeconds(duration)
                         return seconds
                     }
                 })
